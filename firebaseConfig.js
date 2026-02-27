93b52b3d4447f84d3e8bb73731ac54d24a84a886
Kaamil Saib <kaamilsaib@Kaamils-MacBook-Air.local>
2026-02-27 02:36:52 +0200

    everything

---
+++ firebaseConfig.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAPBXBoYXFURJ0pC9T_vkOXvS5h2fPMXDA",
+  authDomain: "miniproject-4c4a6.firebaseapp.com",
+  projectId: "miniproject-4c4a6",
+  storageBucket: "miniproject-4c4a6.firebasestorage.app",
+  messagingSenderId: "434475975210",
+  appId: "1:434475975210:web:8f0f8f89510fe8979eb953"
+};
